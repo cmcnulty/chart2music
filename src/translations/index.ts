a6233be2bb2468ebd8993c5caf2370258b0dd210
Charles McNulty <charles.mcnulty@tasconline.com>
2025-12-31 01:24:00 -0600

    undo whitespace changes

--- src/translations/index.ts
+++ src/translations/index.ts
@@ -12,9 +12,9 @@ import hmn from "./hmn";
  * A dictionary of messages, which will be used by the i18n formatter
  */
 export type translationDict = Record<string, string>;
-
 /**
  * Entities to be interpolated into translations. For example, in the ICU message "hello {world}", the entity is "world".
  */
 export type translateEvaluators = Record<string, string | number | boolean>;
+
 export { en, de, es, fr, it, hmn };
